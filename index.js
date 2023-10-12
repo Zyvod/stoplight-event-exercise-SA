6a996d77fc3b00707b2e1bdd25f516479516fb8c
Alexander_Fields <Zyvod1300@yahoo.com>
2023-10-12 15:36:33 -0500

    Dis is De Way to Stop

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 (function() {
   'use strict';
 
-  // YOUR CODE HERE
+var stopBtn = document.getElementById('stopButton')
+
+var goBtn = document.getElementById('goButton')
+
+var slowBtn = document.getElementById('slowButton')
+
+
+goBtn.addEventListener('click',function() {
+  let goLight = document.getElementById('goLight')
+  goLight.classList.toggle('go')
+})
+
+slowBtn.addEventListener('click',function() {
+  let slowLight = document.getElementById('slowLight')
+  slowLight.classList.toggle('slow')
+})
+
+stopBtn.addEventListener('click',function() {
+  let stopLight = document.getElementById('stopLight')
+  stopLight.classList.toggle('stop')
+})
+
+
 })();
